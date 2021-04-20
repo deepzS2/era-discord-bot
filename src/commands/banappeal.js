c967ca5b701df28225ee6ed62dba91da43106661
deepzS2 <alanbackman10@hotmail.com>
2021-04-19 23:19:59 -0300

    Bug fix: banappeal client 🐞

--- src/commands/banappeal.js
+++ src/commands/banappeal.js
@@ -1,36 +1,42 @@
-const { ERA_DISCORD } = require("../config");
-const { MessageEmbed } = require("discord.js");
+const { ERA_DISCORD } = require("../config")
+const { MessageEmbed } = require("discord.js")
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-// needs fixxx
+const Discord = require("discord.js")
 
 module.exports = {
   name: "banappeal",
   description: "ban appeal command.",
-  args: [ 
+  args: [
     { name: "`<name>`", value: "*your ingame nickname*", inline: true },
-    { name: "`<steamid>`", value: "*your current steam account link*", inline: true, },
-    { name: "`<reason>`", value: "*reason on why we should unban you*", inline: true, }
+    {
+      name: "`<steamid>`",
+      value: "*your current steam account link*",
+      inline: true,
+    },
+    {
+      name: "`<reason>`",
+      value: "*reason on why we should unban you*",
+      inline: true,
+    },
   ],
   usage: "e!banappeal <name> <steamid> <reason>",
   cooldown: 10, //100000000000000
   disabled: true,
-  execute(message, args) {
-
+  execute(message, args, client) {
     if (message.channel.type != "dm") {
-      message.delete();
-      message.author.send("hi, use that commands here, check **`e!help banappeal`**");
-      return;
+      message.delete()
+      message.author.send(
+        "hi, use that commands here, check **`e!help banappeal`**"
+      )
+      return
     }
 
     // check if player is banned from mysql
     // check if player alerady has req a ban appeal in the past or one atm
 
-    const playername = args.shift();
-    const steamlink = args.shift();
-    const reason = args.join(" ");
+    const playername = args.shift()
+    const steamlink = args.shift()
+    const reason = args.join(" ")
 
     const embed = new MessageEmbed()
       .setColor("#0099ff")
@@ -47,25 +53,24 @@ module.exports = {
         { name: ":user:  Player name:", value: playername },
         { name: ":steam:  Steam link:", value: steamlink },
         { name: ":speech_balloon:  Reason:", value: reason }
-      );
+      )
 
     setTimeout(() => {
-      const server = client.guilds.cache.get(ERA_DISCORD);
-      console.log(ERA_DISCORD);
-      server.channels.create("banappeal-" + playername); // fix permissions for admins only and player that requested!
+      const server = client.guilds.cache.get(ERA_DISCORD)
+      // console.log(ERA_DISCORD);
+      server.channels.create("banappeal-" + playername) // fix permissions for admins only and player that requested!
       setTimeout(() => {
         const channel = server.channels.cache.find(
           (guild) => guild.name === "banappeal-" + playername
-        );
-        channel.send(embed);
-        channel.send("@here ```Unban " + playername + " ?```");
-      }, 2000);
-    }, 1000);
+        )
+        channel.send(embed)
+        channel.send("@here ```Unban " + playername + " ?```")
+      }, 2000)
+    }, 1000)
 
     //create timer of 2 days or atleast X votes?
-
   },
-};
+}
 
 /* todo
 function cmd_banappeal(msg, args) {
@@ -73,3 +78,4 @@ function cmd_banappeal(msg, args) {
 
   
 } */
+
--- src/commands/help.js
+++ src/commands/help.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-const { PREFIX } = require("../config");
-=======
-const { prefix } = require("../config");
->>>>>>> 90a25f2... feature: embeds ✨
-const { MessageEmbed } = require("discord.js");
+const { PREFIX } = require("../config")
+const { prefix } = require("../config")
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
   //fields
@@ -18,100 +15,99 @@ module.exports = {
   ],
   usage: "e!help (<command>)",
   cooldown: 3,
-  footer: "USING ONLY e!help WILL SHOW YOU ALL OUR COMMANDS; NOTE: < > = REQUIRED , [ ] = OPTIONAL",
+  footer:
+    "USING ONLY e!help WILL SHOW YOU ALL OUR COMMANDS; NOTE: < > = REQUIRED , [ ] = OPTIONAL",
   disabled: false,
 
   //run: e!help or e!help <command>
-  execute(message, args) {
-<<<<<<< HEAD
+  execute(message, args, client) {
     // all our bot commands
-    const { commands } = message.client;
+    const { commands } = message.client
 
     // create cool embed with all the commands/individual command info
     const embed = new MessageEmbed()
       .setColor("#fc8c03")
       .setThumbnail("https://i.imgur.com/vrRImoI.png")
-      //.setAuthor("ERA DISCORD BOT", "https://i.imgur.com/hOuIomW.png", "https://steamcommunity.com/groups/EraSurfCommunity");
+    //.setAuthor("ERA DISCORD BOT", "https://i.imgur.com/hOuIomW.png", "https://steamcommunity.com/groups/EraSurfCommunity");
 
     // if e!help was used
     if (!args.length) {
-      embed.setTitle("🔹 Commands").setFooter('FOR SPECIFIC COMMAND USAGE, TRY e!help <command>');
+      embed
+        .setTitle("🔹 Commands")
+        .setFooter("FOR SPECIFIC COMMAND USAGE, TRY e!help <command>")
       commands.forEach((command) => {
-        if (command.name) embed.addField(`${PREFIX}${command.name}`, command.description);
-      });
-=======
-    // The client.commands collection
-    const { commands } = message.client;
+        if (command.name)
+          embed.addField(`${PREFIX}${command.name}`, command.description)
+      })
+      // The client.commands collection
+      const { commands } = message.client
 
-    // The embed
-    const embed = new MessageEmbed()
-      .setColor("#fc8c03")
-      .setAuthor(
-        "ERA DISCORD BOT",
-        "https://i.imgur.com/hOuIomW.png",
-        "https://steamcommunity.com/groups/EraSurfCommunity"
-      )
-      .setThumbnail("https://i.imgur.com/vrRImoI.png");
+      // The embed
+      const embed = new MessageEmbed()
+        .setColor("#fc8c03")
+        .setAuthor(
+          "ERA DISCORD BOT",
+          "https://i.imgur.com/hOuIomW.png",
+          "https://steamcommunity.com/groups/EraSurfCommunity"
+        )
+        .setThumbnail("https://i.imgur.com/vrRImoI.png")
 
-    // If display all commands
-    if (!args.length) {
-      embed.setTitle("Commands");
+      // If display all commands
+      if (!args.length) {
+        embed.setTitle("Commands")
 
-      commands.forEach((command) => {
-        embed.addField(`${prefix}${commandName}`, command.description);
-      });
-
-      return message.author
-        .send(embed)
-        .then(() => {
-          if (message.channel.type === "dm") return;
-
-          message.reply(`I\'ve sent you a DM with all my commands!`);
+        commands.forEach((command) => {
+          embed.addField(`${prefix}${commandName}`, command.description)
         })
-        .catch((error) => {
-          console.error(
-            `Could not send help DM to ${message.author.tag}.\n`,
-            error
-          );
-          message.reply(
-            `It seems like I can\'t DM you! Do you have DMs disabled?`
-          );
-        });
-    }
->>>>>>> 90a25f2... feature: embeds ✨
 
-      return message.reply(embed);
+        return message.author
+          .send(embed)
+          .then(() => {
+            if (message.channel.type === "dm") return
+
+            message.reply(`I\'ve sent you a DM with all my commands!`)
+          })
+          .catch((error) => {
+            console.error(
+              `Could not send help DM to ${message.author.tag}.\n`,
+              error
+            )
+            message.reply(
+              `It seems like I can\'t DM you! Do you have DMs disabled?`
+            )
+          })
+      }
+
+      return message.reply(embed)
     }
     //else, e!help <command> was used so display info about the invidual command
 
     // get <command>
-    const cmd_arg = args[0].toLowerCase();
-    const command = commands.get(cmd_arg);
+    const cmd_arg = args[0].toLowerCase()
+    const command = commands.get(cmd_arg)
 
     /* 
     // get aliases maybe? || commands.find(c => c.aliases && c.aliases.includes(name)); 
     */
 
-<<<<<<< HEAD
-    if (!command) return message.reply(`that\'s not a valid command  😔`);
+    if (!command) return message.reply(`that\'s not a valid command  😔`)
 
-    embed.setTitle("🔹 " + command.name);
-    embed.setDescription(command.description + '```yaml\nUsage: ' + command.usage + '\n```');
-=======
-    embed.setTitle(command.name);
+    embed.setTitle("🔹 " + command.name)
+    embed.setDescription(
+      command.description + "```yaml\nUsage: " + command.usage + "\n```"
+    )
+    embed.setTitle(command.name)
 
     if (command.description);
-    embed.setDescription(command.description);
+    embed.setDescription(command.description)
 
     // if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
     // if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
->>>>>>> 90a25f2... feature: embeds ✨
 
     //if (command.cooldown) embed.addField("Cooldown time", `${command.cooldown} seconds`);
-    if (command.args)     embed.addFields(command.args);
-    if (command.footer)   embed.setFooter(command.footer);
+    if (command.args) embed.addFields(command.args)
+    if (command.footer) embed.setFooter(command.footer)
 
-    message.channel.send(embed);
+    message.channel.send(embed)
   },
-};
-
+}
